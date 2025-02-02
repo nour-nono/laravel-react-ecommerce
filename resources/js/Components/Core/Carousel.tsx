b69bd1218c8f01b7bc68ddfdc7412fe490b41528
nono <nour98nour@gmail.com>
2025-02-02 19:35:27 +0200

    Enhance carousel component with image selection and update styles for WYSIWYG output

--- resources/js/Components/Core/Carousel.tsx
+++ resources/js/Components/Core/Carousel.tsx
@@ -1,30 +1,34 @@
 import { Image } from "@/types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Carousel({ images }: { images: Image[] }) {
+  const [selectedImage, setSelectedImage] = useState<Image>(images[0]);
+  useEffect(() => {
+    setSelectedImage(images[0]);
+  }, images);
   return (
     <>
       <div className="flex items-start gap-8">
         <div className="flex flex-col items-center gap-2 py-2">
           {images.map((image, i) => (
-            <a
-              href={"#item" + i}
+            <button
+              onClick={ _ => setSelectedImage(image)}
               key={image.id}
-              className="border-2 hover:border-blue-500"
+              className={
+                "border-2 " +
+                (selectedImage.id === image.id
+                  ? "border-blue-500"
+                  : "hover:border-blue-500")
+              }
             >
               <img src={image.thumb} alt="" className="w-[50px]" />
-            </a>
+            </button>
           ))}
         </div>
         <div className="carousel w-full">
-          {images.map((image, i) => (
-            <div id={"item" + i} key={image.id} className="carousel-item w-full">
-              <img
-                src={image.large}
-                alt=""
-                className="w-full" />
-            </div>
-          ))}
+          <div className="carousel-item w-full">
+            <img src={selectedImage.large} alt="" className="w-full" />
+          </div>
         </div>
       </div>
     </>
